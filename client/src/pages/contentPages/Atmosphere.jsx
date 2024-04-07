021c2d044f3a393395f20354103dad01ce0c27e4
TejaswiniMahur <tjsmahur@gmail.com>
2024-04-08 01:42:29 +0530

    half features added atmosphere not working

--- client/src/pages/contentPages/Atmosphere.jsx
+++ client/src/pages/contentPages/Atmosphere.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Card from "../../components/Card.jsx";
+import { data } from "../../utils/data.jsx";
+
 
 export default function Atmosphere() {
   return (
-    <div>Atmosphere</div>
-  )
+    <div className="flex flex-col font-comic text-white font-semibold ">
+      <div
+        style={{ backgroundImage: "url('/solar7.jpg')" }}
+        className="bg-black bg text-[3vw] flex justify-center p-[1vw]"
+      >
+        <h1>The Layer of Atmosphere</h1>
+      </div>
+
+      {data.map((data,index) => (
+        <div key={index}>
+          <Card data={data}/>
+        </div>
+      ))}
+    </div>
+  );
 }
